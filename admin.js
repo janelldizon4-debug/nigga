7182ab324acc842d97a9287f46f2e6da21b8d1fc
janelldizon4-debug <janelldizon4@gmail.com>
2026-01-15 15:48:27 +0800

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,6 +1,8 @@
 
 function loadAdminPanel() {
     const tbody = document.getElementById("userTable");
+    if (!tbody) return;
+
     tbody.innerHTML = "";
 
     REGISTERED_KEYS.forEach((user, index) => {
@@ -11,11 +13,11 @@ function loadAdminPanel() {
             <td>${user.accessKey}</td>
             <td>${user.subscription}</td>
             <td>${user.expires}</td>
-            <td>${user.revoked}</td>
-            <td>${user.stop}</td>
+            <td>${user.revoked ? 'Yes' : 'No'}</td>
+            <td>${user.stopped ? 'Yes' : 'No'}</td>
             <td>
-                <button onclick="revokeKey(${index})">Revoke</button>
-                <button onclick="toggleStop(${index})">Stop/Resume</button>
+                <button onclick="revokeKey(${index})" style="margin-right:5px;">Revoke</button>
+                <button onclick="stopKey(${index})">Stop</button>
             </td>
         `;
 
@@ -26,16 +28,31 @@ function loadAdminPanel() {
 
 function revokeKey(index) {
     const user = REGISTERED_KEYS[index];
-    if(confirm(`Are you sure you want to revoke ${user.name}'s key?`)) {
+    if (!user) return;
+
+    if (confirm(`Are you sure you want to revoke ${user.name}'s key?`)) {
         user.revoked = true;
-        loadAdminPanel();
+        loadAdminPanel(); 
         alert(`${user.name}'s key has been revoked!`);
     }
 }
 
 
-function toggleStop(index) {
+function stopKey(index) {
     const user = REGISTERED_KEYS[index];
-    user.stop = !user.stop;
-    loadAdminPanel();
+    if (!user) return;
+
+    if (confirm(`Are you sure you want to stop ${user.name}'s subscription?`)) {
+        user.stopped = true;
+        loadAdminPanel(); // refresh table
+        alert(`${user.name}'s subscription has been stopped!`);
+    }
 }
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const adminPanel = document.getElementById("adminPanel");
+    if (adminPanel && adminPanel.style.display === "block") {
+        loadAdminPanel();
+    }
+});
